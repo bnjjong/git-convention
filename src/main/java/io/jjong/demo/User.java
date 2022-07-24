9780415bc998497b843e3adeb28f2f53465d0c62
Jordan <jongsang@bigin.io>
2022-07-24 22:42:01 +0900

    Initial commit

---
+++ src/main/java/io/jjong/demo/User.java
@@ -0,0 +1,26 @@
+/*
+ * Created By dogfootmaster@gmail.com on 2022
+ * This program is free software
+ *
+ * @author <a href=“mailto:dogfootmaster@gmail.com“>Jongsang Han</a>
+ * @since 2022/07/24
+ */
+
+package io.jjong.demo;
+
+/**
+ * create on 2022/07/24. create by IntelliJ IDEA.
+ *
+ * <p> 클래스 설명 </p>
+ * <p> {@link } and {@link }관련 클래스 </p>
+ *
+ * @author Jongsang Han
+ * @version 1.0
+ * @see
+ * @since 1.0
+ */
+public class User {
+  private String name;
+  private int age;
+
+}
